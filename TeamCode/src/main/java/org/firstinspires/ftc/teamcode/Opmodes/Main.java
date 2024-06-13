0b18ea68a046918797454a2402b4a486c68385c4
mhiuuu <maitrunghieu2287@gmail.com>
2024-06-13 22:58:46 +0700

    OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Main.java
@@ -7,13 +7,24 @@ import org.firstinspires.ftc.teamcode.Subsystems.Drivebase;
 
 @TeleOp(name = "Main")
 public class Main extends LinearOpMode {
-    private Drivebase drivetrain;
+    private Drivebase drive;
     public void runOpMode() {
-        drivetrain = new Drivebase(hardwareMap);
+        drive = new Drivebase(hardwareMap);
 
         waitForStart();
         while (opModeIsActive()) {
-            drivetrain.setDrivePower(-gamepad1.left_stick_y,-gamepad1.right_stick_y);
+            double y = -gamepad1.left_stick_y;
+            double rx = gamepad1.right_stick_x;
+            double max = Math.abs(y) + Math.abs(rx);
+            max = Math.max(max, 1);
+            double pLeft = (y + rx) / max;
+            double pRight = (y - rx) / max;
+            drive.setDrivePower(pLeft, pRight);
+            if(gamepad1.circle) {
+                drive.setClimber(0.7);
+            } else if(gamepad1.square) {
+                drive.setClimber(-0.7);
+            }
         }
     }
 }
